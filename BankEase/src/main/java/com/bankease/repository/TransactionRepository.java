af53068911dd894cd6a15667e7d9456205893a2c
adarshdixit989 <119063821+adarshdixit989@users.noreply.github.com>
2026-09-07 09:34:25 +0530

    Add BankEase TransactionRepository.java

---
+++ BankEase/src/main/java/com/bankease/repository/TransactionRepository.java
@@ -0,0 +1,1 @@
+package com.bankease.repository; import com.bankease.entity.Transaction; import org.springframework.data.domain.*; import org.springframework.data.jpa.repository.JpaRepository; public interface TransactionRepository extends JpaRepository<Transaction,Long>{Page<Transaction> findByAccountIdOrderByCreatedAtDesc(Long accountId,Pageable pageable);}
